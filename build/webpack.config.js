b246e3a7ba6844936c5dfc870c1dff0a4c2daf78
lei-zhu <lei.zhu@live.cn>
2020-07-01 10:51:44 +0800

    chore: add webpack plugins

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackBar = require("webpackbar");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -29,6 +34,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "build/tpl/index.html"
-    })
+    }),
+    new WebpackBar({
+      name: "my-react-project",
+      color: "#61dafb"
+    }),
+    new FriendlyErrorsWebpackPlugin(),
+    new CaseSensitivePathsPlugin(),
+    new CircularDependencyPlugin({ 
+      exclude: /node_modules/, 
+      include: /src/, 
+      failOnError: true, 
+      allowAsyncCycles: false, 
+      cwd: process.cwd(), 
+    }),
+    new CleanWebpackPlugin(),
   ],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const render = () => {
-  ReactDOM.render(<div>hello world</div>, document.querySelector("#app"));
+  ReactDOM.render(<div>hello world</div>, document.querySelector("#root"));
 };
 
 render();
